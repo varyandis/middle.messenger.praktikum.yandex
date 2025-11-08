8a72de045f4e651940690aca293088907c234d55
Александра <varyandis@yandex.ru>
2025-11-08 14:55:27 +0300

    add login

---
+++ src/pages/login/renderLogin.ts
@@ -0,0 +1,7 @@
+import Handlebars from 'handlebars'
+import template from './login.hbs?raw'
+
+const loginTemplate = Handlebars.compile(template)
+const html = loginTemplate({})
+
+document.querySelector('.page-centered')!.innerHTML = html
