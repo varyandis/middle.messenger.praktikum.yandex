4f8e09237773767c3e98725ebe18ac8be55a30b7
Александра <varyandis@yandex.ru>
2025-11-08 14:55:11 +0300

    add style

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,3 @@
 import { defineConfig } from 'vite'
-import handlebars from 'vite-plugin-handlebars'
 
-export default defineConfig({
-  plugins: [
-    handlebars({
-      context: {
-        title: "Шаблон",
-        message: 'Привет'
-      }
-    })
-  ],
-
-})
+export default defineConfig({})
