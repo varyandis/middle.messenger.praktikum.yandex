bdbaae114216339a86072f43d5439a72791fe864
gerasimova alexandra <gerasimova_a_n@ntik.ru>
2025-11-10 13:27:03 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,8 @@
-import { defineConfig } from 'vite';
-import { resolve } from 'path';
+import { defineConfig } from 'vite'
+import { fileURLToPath } from 'node:url'
+import { dirname, resolve } from 'node:path'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   server: { port: 3000, strictPort: true },
@@ -7,14 +10,14 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        index:       resolve(__dirname, 'index.html'),
-        login:       resolve(__dirname, 'src/pages/login/login.html'),
-        registration:resolve(__dirname, 'src/pages/registration/registration.html'),
-        chats:       resolve(__dirname, 'src/pages/chats/chats.html'),
-        profile:     resolve(__dirname, 'src/pages/profile/profile.html'),
-        error404:    resolve(__dirname, 'src/pages/error404/error404.html'),
-        error500:    resolve(__dirname, 'src/pages/error500/error500.html'),
-      },
-    },
-  },
-});
+        index:        resolve(__dirname, 'index.html'),
+        login:        resolve(__dirname, 'src/pages/login/login.html'),
+        registration: resolve(__dirname, 'src/pages/registration/registration.html'),
+        chats:        resolve(__dirname, 'src/pages/chats/chats.html'),
+        profile:      resolve(__dirname, 'src/pages/profile/profile.html'),
+        error404:     resolve(__dirname, 'src/pages/error404/error404.html'),
+        error500:     resolve(__dirname, 'src/pages/error500/error500.html')
+      }
+    }
+  }
+})
